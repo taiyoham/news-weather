7e8f087111fbae2f4491a978b36860bfc9063282
Taiyo Hamamoto <taiyo.ocean5123ht@gmail.com>
2024-03-21 21:19:38 +0900

    アプリの完成

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import NewsScreen from "./screens/NewsScreen";
+import DetailScreen from "./screens/DetailScreen";
+import WeatherScreen from "./screens/WeatherScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const NewsStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="ニュース" component={NewsScreen} />
+      <Stack.Screen name="詳細ページ" component={DetailScreen} />
+    </Stack.Navigator>
   );
-}
+};
+
+const WeatherStack = () => {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen name="天気予報" component={WeatherScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+    if (route.name === "ニュース") {
+      iconName = "newspaper-o";
+    } else if (route.name === "天気予報") {
+      iconName = "sun-o";
+    }
+
+    // You can return any component that you like here!
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+});
+
+export default App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOption}>
+        <Tab.Screen name="ニュース" component={NewsStack}/>
+        <Tab.Screen name="天気予報" component={WeatherStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    backgroundColor: "red",
   },
 });
